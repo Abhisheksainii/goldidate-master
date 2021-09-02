8d41fad746f9418b5acc854a89be0015ec849a05
Abhishek Saini <78199221+Abhisheksainii@users.noreply.github.com>
2021-09-02 08:52:14 +0530

    update added

---
+++ lib/screens/Home/Update/Bio.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Bio extends StatefulWidget {
+  @override
+  _BioState createState() => _BioState();
+}
+
+class _BioState extends State<Bio> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
